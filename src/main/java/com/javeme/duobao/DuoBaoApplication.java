47edb0b5d024f38ceccc4f53321713e9f3dd5725
BEINTL\weiting.lee <weiting.lee@beintl.com>
2026-04-18 11:09:23 +0800

    1st v

---
+++ src/main/java/com/javeme/duobao/DuoBaoApplication.java
@@ -0,0 +1,13 @@
+package com.javeme.duobao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoBaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuoBaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javeme/duobao/DuoBaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javeme.duobao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoBaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
